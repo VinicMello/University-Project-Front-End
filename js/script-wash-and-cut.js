514989d208ecf4af81adc60f345047ce216654e4
vinicius.pedroso <vinicius.pedroso@madeiramadeira.com.br>
2024-06-04 19:22:48 -0300

    organizing href in pages

---
+++ js/script-wash-and-cut.js
@@ -0,0 +1,11 @@
+console.log('teste')
+
+//about owner the dog
+const inputName = document.getElementById("name");
+const inputAddress = document.getElementById("address");
+const inputCep = document.getElementById("cep");
+const inputEmail = document.getElementById("email");
+const inputTelephone = document.getElementById("telephone");
+
+//about the dog
+const inputNameDog = document.getElementById("name-dog");
